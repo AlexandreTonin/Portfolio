e64898ccb7c4c5df8c783948b472af491b37be38
Alexandre Tonin <107586000+AlexandreTonin@users.noreply.github.com>
2023-10-22 15:28:33 -0300

    Updating skill tooltip styles

--- portfolio-webpage/src/components/Skill/Skill.tsx
+++ portfolio-webpage/src/components/Skill/Skill.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Skill = (props: any) => {
     return (
-        <Tooltip label={props.tooltipLabel} fontSize='md' className='dark:bg-sky-400 bg-sky-600 font-black text-white px-2 rounded-full dark:bg-opacity-20 bg-opacity-20 border lg:border-sky-400 border-sky-600'>
+        <Tooltip label={props.tooltipLabel} fontSize='md' className='dark:bg-sky-400 bg-sky-600 font-black text-gray-600 dark:text-white px-2 rounded-full dark:bg-opacity-20 bg-opacity-20 border lg:border-sky-400 border-sky-600'>
             <div className='bg-opacity-20 border rounded p-4 border-gray-300 bg-gray-200 dark:border-gray-800 dark:bg-gray-800 dark:bg-opacity-20 cursor-pointer'>
                 <img height="30" width="40" src={props.imgUrl} />
             </div>
